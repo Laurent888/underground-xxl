003c6b29e7ee6a2c01192cead991b2acca890068
Laurent Tram <laurent.tram@rocker.com>
2024-09-10 16:58:32 +0300

    ugxxl 2024 schedule

--- src/pages/program.tsx
+++ src/pages/program.tsx
@@ -1,6 +1,9 @@
 import HeroImage from '@/components/HeroImage'
 import TicketButtonNavigation from '@/components/TicketButtonNavigation'
 import Image from 'next/image'
+import friday from '../public/schedule-friday.jpg'
+import saturday from '../public/schedule-sat.jpg'
+import sunday from '../public/schedule-sun.jpg'
 
 export default function Program() {
   return (
@@ -8,44 +11,60 @@ export default function Program() {
       <HeroImage title="Program" />
 
       <section className="container mt-12 mx-auto flex flex-col items-center md:items-start mb-12">
-        <h3 className="text-3xl font-bold italic mb-1 text-center mx-2">PROGRAM DETAILS</h3>
-        <p className="mb-12 text-center mx-2 text-stone-500">
-          This is a preliminary program. It will be subject to change
-        </p>
+        <h3 className="text-3xl font-bold italic mb-1 text-center mx-2 mb-10">PROGRAM DETAILS</h3>
 
         <div className="mb-12 px-2">
-          <p className="mb-2 text-2xl font-bold italic">Friday</p>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">20:00 - 22:00</p>
-            <p>Workshops</p>
-          </div>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">22:00 - 04:00</p>
-            <p>Social</p>
+          <div className="pb-8 px-4">
+            <p className="mb-2 text-2xl font-bold italic">Friday</p>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">20:30 - 22:30</p>
+              <p>Workshops</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">22:00 - 04:00</p>
+              <p>Social</p>
+            </div>
+
+            <p className="mb-2 text-2xl font-bold italic mt-4 ">Saturday</p>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">11:00 - 17:30</p>
+              <p>Workshops</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">21:30 - 22:30</p>
+              <p>Pre-party workshop</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">22:30 - 05:00</p>
+              <p>Social</p>
+            </div>
+
+            <p className="mb-2 text-2xl font-bold italic mt-4 ">Sunday</p>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">13:00 - 18:30</p>
+              <p>Workshops</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="font-bold mr-2">18:30 - 22:30</p>
+              <p>Social</p>
+            </div>
           </div>
 
-          <p className="mb-2 text-2xl font-bold italic mt-4 ">Saturday</p>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">12:00 - 18:00</p>
-            <p>Workshops</p>
-          </div>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">21:00 - 22:00</p>
-            <p>Pre-party workshop</p>
-          </div>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">22:00 - 05:00</p>
-            <p>Social</p>
-          </div>
+          <div className="flex flex-col md:flex-row">
+            <div className="w-full px-4 mb-6 md:w-1/2 md:mr-4 md:px-0">
+              <Image src={friday} alt="friday schedule" style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
+            </div>
+            <div className="w-full px-4 mb-6 md:w-1/2 md:mr-4 md:px-0">
+              <Image
+                src={saturday}
+                alt="saturday schedule"
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+              />
+            </div>
 
-          <p className="mb-2 text-2xl font-bold italic mt-4 ">Sunday</p>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">13:00 - 19:00</p>
-            <p>Workshops</p>
-          </div>
-          <div className="flex flex-row">
-            <p className="font-bold mr-2">19:00 - 22:30</p>
-            <p>Social</p>
+            <div className="w-full px-4 mb-6 md:w-1/2 md:mr-4 md:px-0">
+              <Image src={sunday} alt="sunday schedule" style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
+            </div>
           </div>
 
           <p className="mb-2 mt-8">• Please bring an ID for the registration.</p>
